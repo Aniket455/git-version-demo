54344ac0dc67c7a63731cabc1e8dbe6e5efb685a
Aniket455 <aniketsgupta0302@gmail.com>
2025-08-11 13:00:18 +0530

    initial commit

---
+++ src/test/java/com/smarte/gitdemo/GitVersionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarte.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitVersionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
